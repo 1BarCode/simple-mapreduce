c09a83dee5ab6b5b4b5e359c82302be24dc0943c
Tai Huynh <69029687+1BarCode@users.noreply.github.com>
2026-05-04 21:58:53 -0700

    minor change

--- include/mr/master/master_config.h
+++ include/mr/master/master_config.h
@@ -12,8 +12,8 @@ struct MasterConfig {
     std::string             work_dir;
     // absl::Duration  heartbeat_interval = absl::Seconds(3);
     // absl::Duration  worker_timeout = absl::Seconds(3);
-    std::chrono::seconds    heartbeat_interval = 3s;
-    std::chrono::seconds    worker_timeout = 3s;
+    std::chrono::seconds    heartbeat_interval = 1s;
+    std::chrono::seconds    worker_timeout = 1s;
     int                     num_workers = 4;
     int                     map_slots_per_worker = 2;
     int                     reduce_slots_per_worker = 1;
